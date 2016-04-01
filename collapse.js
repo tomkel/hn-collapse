8c8340d11597fca62b8299821ed3cb3c1ce0ec71
Tommy Kelly <git@tkel.ly>
2016-04-01 01:12:41 -0400

    Refactor pretty much everything

--- collapse.js
+++ collapse.js
@@ -1,38 +1,34 @@
-// faster than querySelectorAll
-// gives an HTMLCollection instead of a NodeList
-
 // dash, not hyphen
 const minusText = '[–]'
 const plusText = '[+]'
 
 function collapse() {
-  const thisGif = this.parentElement.parentElement.parentElement.previousSibling
-    .previousSibling.firstChild
-  const depth = thisGif.width
 
-  const gifs = document.querySelectorAll('img[src="s.gif"]')
+  this.comment.classList.toggle('collapsed')
+  this.upvote.classList.toggle('invisible')
+
   let hiding
+  if (this.text === minusText) {
+    this.text = plusText
+    hiding = true
+  } else {
+    this.text = minusText
+    hiding = false
+  }
+
+  const depth = this.gif.width
+
+  // this fails when stored as a global for some reason
+  const things = document.querySelector('.comment-tree').querySelectorAll('.athing')
   let found = false
-  for (let gif of gifs) {
-    if (thisGif === gif) {
-      const comment = gif.parentElement.nextSibling.nextSibling.lastChild
-      comment.classList.toggle('collapsed')
-      const upvote = gif.parentElement.nextSibling
-      upvote.classList.toggle('invisible')
-      if (this.text === minusText) {
-        this.text = plusText
-        hiding = true
-      } else {
-        this.text = minusText
-        hiding = false
-      }
+  for (let thing of things) {
+    if (thing === this.thing) {
       found = true
       continue
     }
     if (found) {
-      if (gif.width <= depth) break
-      const thing = gif.parentElement.parentElement.parentElement.parentElement.parentElement
-        .parentElement
+      if (thing.gif.width <= depth) break
+
       if (!thing.timesCollapsed)
         thing.timesCollapsed = 0
       if (hiding) {
@@ -52,14 +48,28 @@ function collapse() {
 }
 
 (() => {
-  const comheads = document.getElementsByClassName('comhead')
+  const commentTree = document.querySelector('.comment-tree')
+  // faster than querySelectorAll
+  // gives an HTMLCollection instead of a NodeList
+  const comheads = commentTree.getElementsByClassName('comhead')
   for (let comhead of comheads) {
-    // there is a span.sitebit.comhead that we don't want
-    if (comhead.classList.length === 2) continue
     const a = document.createElement('a')
     a.text = minusText
     a.href = '#'
-    a.onclick = collapse
+    a.addEventListener('click', collapse)
     comhead.insertBefore(a, comhead.firstChild)
+
+    let thing = a
+    do {
+      thing = thing.parentElement
+    } while (thing.className !== 'athing')
+
+    // setup references
+    a.gif = thing.querySelector('img')
+    a.comment = thing.querySelector('.comment')
+    a.upvote = thing.querySelector('.votearrow').parentElement
+
+    a.thing = thing
+    thing.gif = a.gif
   }
 })()
